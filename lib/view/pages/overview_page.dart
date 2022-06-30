33608883e210d9bbb55bfe509430b070c0cf4871
Jlnrrg <julian.roerig.k@gmail.com>
2022-06-30 20:23:17 +0200

    add theme selection

--- lib/view/pages/overview_page.dart
+++ lib/view/pages/overview_page.dart
@@ -2,6 +2,7 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_issue_viewer/app/theme/theme_mode_notifier.dart';
+import 'package:github_issue_viewer/app/theme/theme_notifier.dart';
 import 'package:github_issue_viewer/domain/theme.dart';
 import 'package:github_issue_viewer/view/router/router.dart';
 import 'package:go_router/go_router.dart';
@@ -11,22 +12,45 @@ class OverviewPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
+    final appTheme = ref.watch(themeProvider);
     final themeMode = ref.watch(themeModeProvider);
 
     return Scaffold(
-      body: Column(
-        children: [
-          const Text('Overview'),
-          TextButton(
-              onPressed: () =>
-                  context.go(context.namedLocation(MyRouter.routeNameIssue)),
-              child: const Text('To Issue')),
-          FlexThemeModeSwitch(
-              themeMode: themeMode,
-              onThemeModeChanged: (m) =>
-                  ref.read(themeModeProvider.notifier).changed(m),
-              flexSchemeData: const AppTheme.aquaBlue().toFlexSchemeData())
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Text('Overview'),
+            TextButton(
+                onPressed: () =>
+                    context.go(context.namedLocation(MyRouter.routeNameIssue)),
+                child: const Text('To Issue')),
+            for (ThemeMode tm in [ThemeMode.light, ThemeMode.dark])
+              Wrap(
+                children: AppTheme.all
+                    .map(
+                      (theme) => IntrinsicWidth(
+                        child: FlexThemeModeOptionButton(
+                            backgroundColor:
+                                tm == ThemeMode.dark ? Colors.grey[850] : null,
+                            flexSchemeColor: tm == ThemeMode.dark
+                                ? theme.toFlexSchemeData().dark
+                                : theme.toFlexSchemeData().light,
+                            selected: theme == appTheme && tm == themeMode,
+                            onSelect: () {
+                              ref.read(themeProvider.notifier).changed(theme);
+                              ref.read(themeModeProvider.notifier).changed(tm);
+                            }),
+                      ),
+                    )
+                    .toList(),
+              ),
+            FlexThemeModeSwitch(
+                themeMode: themeMode,
+                onThemeModeChanged: (m) =>
+                    ref.read(themeModeProvider.notifier).changed(m),
+                flexSchemeData: const AppTheme.aquaBlue().toFlexSchemeData())
+          ],
+        ),
       ),
     );
   }
